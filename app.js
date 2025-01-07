503ca19fdf564e89873cd27502fb2cb1a67b918e
RavinduPerera29 <ravindup@healthreconconnect.com>
2025-01-07 20:53:24 +0530

    Add sample tests

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const express = require("express");
-const multer = require("multer");
-const queries = require("./queries.js");
+const userRoutes = require("../express/routes/api");
 
 const app = express();
-const upload = multer({ dest: "/upload-file" });
 
 const PORT = 4000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/upload-file", upload.single("file"), queries.createFileData);
-
-app.post("/set-data", (req, res) => {
-  const { name } = req.body;
-  res.set("Content-Type", "Application/json");
-  res.status(200).send(`${name}`);
-});
-
-app.get("/get-all-data", (req, res) => {
-  queries.getAllFileData(req, res);
-});
-
-app.get("/sample", (req, res) => {
-  res.set("Content-Type", "text/html");
-  res.status(200).send("<h1>Express.js!</h1>");
+app.use("/api", userRoutes);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, (error) => {
